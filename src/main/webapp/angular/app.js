24a167ed01c0ed6d5cd74ec6fc24f1a98c10cc92
lukasz-winiarczyk <lk.winiarczyk@gmail.com>
2014-05-31 12:29:42 +0200

    Zmiany w pom, dodanie arkusza styli, zmiany linkow do resta

--- src/main/webapp/angular/app.js
+++ src/main/webapp/angular/app.js
@@ -1,16 +1,16 @@
 var app = angular.module('student', []);
-
+var appName = "/Student-MVC.Winiarczyk-Ochnio";
 app.controller('MainCtrl', function($scope,$http) {
   $scope.name = "Felipe";
   $scope.pokazStudentow=function(){
-	$http.get('/AngularSpringApp/studenci/lista').
+	$http.get(appName+'/studenci/lista').
                 success(function(data) {
                 $scope.studenci = data;
                 console.log(data);
             });
         }
   $scope.usunStudenta=function(index){
-	  $http.get('/AngularSpringApp/studenci/usunStudenta/'+$scope.studenci[index].id).
+	  $http.get(appName+'/studenci/usunStudenta/'+$scope.studenci[index].id).
       success(function() {
     	  $scope.pokazStudentow();
       });
